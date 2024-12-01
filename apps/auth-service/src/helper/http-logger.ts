321fc31cff45861e1c958db13ce25938243d34b5
Ricky Firmansyah <rickyfirmansyah2707@gmail.com>
2024-12-01 11:38:23 +0700

    fixing http logger auth service

--- apps/auth-service/src/helper/http-logger.ts
+++ apps/auth-service/src/helper/http-logger.ts
@@ -1,18 +1,22 @@
 import { Context } from 'baojs';
 import { Logger } from './logger.js';
 
-export const HttpLogger = (ctx: Context) => {
-  const start = process.hrtime();
+export const HttpLogger = async (ctx: Context) => {
+  const start = performance.now(); // Start measuring time
 
-  if (!ctx.res) {
-    Logger.http({
-      message: `Request | Method: ${ctx.method} | Headers: ${JSON.stringify(ctx.headers)} | URL: ${ctx.originalUrl || ctx.url}`,
-    });
-  } else {
-    const duration = process.hrtime(start);
-    const durationInMs = duration[0] * 1000 + duration[1] / 1e6;
-    Logger.http({
-      message: `Response | Method: ${ctx.method} | URL: ${ctx.url || ctx.url} | Status: ${ctx?.res.status} | Duration: ${durationInMs.toFixed(2)} ms`,
-    });
-  }
+  // Log the incoming request
+  Logger.http({
+    message: `Request | Method: ${ctx.method} | Headers: ${JSON.stringify(ctx.headers)} | URL: ${ctx.url}`,
+  });
+
+  // Wait for the response to complete
+  await ctx.res;
+
+  const end = performance.now(); // End measuring time
+  const durationInMs = end - start;
+
+  // Log the outgoing response
+  Logger.http({
+    message: `Response | Method: ${ctx.method} | URL: ${ctx.url} | Status: ${ctx?.res?.status} | Duration: ${durationInMs.toFixed(2)} ms`,
+  });
 };
