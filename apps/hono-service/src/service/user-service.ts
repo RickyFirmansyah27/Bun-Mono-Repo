692e71925bcb07574fe129ddcfcf0cd8c2c39ae6
Ricky Firmansyah <rickyfirmansyah2707@gmail.com>
2025-01-04 10:32:20 +0700

    fix hono controller

--- apps/hono-service/src/service/user-service.ts
+++ apps/hono-service/src/service/user-service.ts
@@ -4,7 +4,10 @@ export interface User {
   email: string;
 }
 
-const users: User[] = [];
+const users: User[] = [
+    { id: 1, name: 'John Doe', email: 'john.doe@example.com' },
+    { id: 2, name: 'Jane Smith', email: 'jane.smith@example.com' }
+  ];
 export const getAllUsers = (): User[] => users;
 
 export const addUser = (user: Omit<User, 'id'>): User => {
